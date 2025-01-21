91e15125dbb7dd29111c783809cebadcdc86e536
Kento-8 <kentafanadorpin@gmail.com>
2025-01-20 19:07:47 -0500

    Actualización finally

--- src/Validator.java
+++ src/Validator.java
@@ -1,8 +1,6 @@
 import java.io.File;
 
 public class Validator {
-
-    // Método para validar la existencia de un archivo
     public boolean validateFile(String fileName) {
         File file = new File(fileName);
         return file.exists() && file.isFile();
